264c09721ad666783f8fdaf6c59028065f8ef3c8
KambojRakesh <rakeshkamboj91@gmail.com>
2022-05-30 04:02:30 -0400

    work

--- algo-cb/6_Binary_Search_Tree.py
+++ algo-cb/6_Binary_Search_Tree.py
@@ -1,41 +1,102 @@
-class Tree:
+class BST:
     def __init__(self, data):
-        self.parent = None
-        self.children = []
+        self.right = None
+        self.left = None
         self.data = data
 
-    def addd_children(self, child):
-        child.parent = self
-        self.children.append(child)
+    def add_child(self, data):
+        if self.data == data:
+            return
 
-    def level(self):
-        p = self.parent
-        level = 0
+        if data < self.data:
+            if self.left:
+                return self.left.add_child(data)
+            else:
+                self.left = BST(data)
 
-        while p:
-            p = p.parent
-            level += 1
+        if data > self.data:
+            if self.right:
+                return self.right.add_child(data)
+            else:
+                self.right = BST(data)
 
-        return level
+    def in_order_traverse(self):
+        elements = []
 
-    def print_tree(self):
-        spaces = " " * self.level() * 3
-        prefix = spaces + "|---" if self.parent else ""
-        print(prefix + self.data)
+        if self.left and self.data:
+            elements += self.left.in_order_traverse()
 
-        if self.children:
-            for child in self.children:
-                child.print_tree()
+        elements.append(self.data)
 
-def build_tree():
-    root = Tree("Electronics")
-    tv = Tree("TV")
-    mobiles = Tree("Mobiles")
+        if self.right and self.data:
+            elements += self.right.in_order_traverse()
 
-    root.addd_children(tv)
-    root.addd_children(mobiles)
+        return elements
 
-    root.print_tree()
+    def search(self, val):
+        if self.data == val:
+            return True
+
+        if val < self.data and self.left:
+                return self.left.search(val)
+        if val > self.data and self.right:
+                return self.right.search(val)
+
+        return False
+
+    def search_min(self):
+        if self.left is None:
+            return self.data
+
+        if self.left:
+            return self.left.search_min()
+
+    def pre_order_traversal(self):
+        elements = []
+
+        elements.append(self.data)
+
+        if self.left and self.data:
+            elements.append(self.left.data)
+            self.left.in_order_traverse()
+
+        if self.right and self.data:
+            elements.append(self.right.data)
+            self.right.in_order_traverse()
+
+        return elements
+
+    def post_order_traversal(self):
+        elements = []
+        if self.left and self.data:
+            elements += self.left.in_order_traverse()
+
+        if self.right and self.data:
+            elements += self.right.in_order_traverse()
+
+        elements.append(self.data)
+        return elements
+
+def build_tree(elements):
+    root = BST(elements[0])
+
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+
+    return root
+
 
 if __name__=="__main__":
-    build_tree()
+    elements = [17, 14, 7, 11,8, 20, 15, 23, 27, 28]
+    root = build_tree(elements)
+    elements = root.in_order_traverse()
+    print("Inorder : ", elements)
+    print(root.search(7))
+
+    print(root.search_min())
+
+    elements = root.pre_order_traversal()
+    print("Preorder : ",elements)
+
+    elements = root.post_order_traversal()
+    print("Postorder : ",elements)
---
+++ algo-cb/7_Graph.py
@@ -0,0 +1,60 @@
+class Graph:
+    def __init__(self, edges):
+        self.edges = edges
+        self.graph_dic = {}
+        for start, end in self.edges:
+            if start in self.graph_dic:
+                self.graph_dic[start].append(end)
+            else:
+                self.graph_dic[start] = [end]
+
+        print("Graph Dict:", self.graph_dic)
+
+    def get_path(self, start, end, path =[]):
+        path = path + [start]
+        if start == end:
+            return [path]
+
+        if start not in self.graph_dic:
+            return []
+
+        paths = []
+        for node in self.graph_dic[start]:
+            if node not in path:
+                new_paths = self.get_path(node, end, path)
+                for p in new_paths:
+                    paths.append(p)
+
+        return paths
+
+    def get_shortest_path(self, start, end, path =[]):
+        path = path + [start]
+        if start == end:
+            return path
+
+        if start not in self.graph_dic:
+            return None
+
+        shortest_path = None
+        for node in self.graph_dic[start]:
+            if node not in path:
+                sp = self.get_shortest_path(node, end, path)
+                if sp:
+                    if shortest_path is None or len(sp) < len(shortest_path):
+                        shortest_path = sp
+
+        return shortest_path
+if __name__=="__main__":
+    routes = [
+        ("Mumbai", "Paris"),
+        ("Mumbai", "Dubai"),
+        ("Paris", "Dubai"),
+        ("Paris", "New York"),
+        ("Dubai", "New York"),
+        ("New York", "Toronto"),
+    ]
+
+    rout_graph = Graph(routes)
+    #paths = rout_graph.get_path("Mumbai", "New York")
+    shortest_path = rout_graph.get_shortest_path("Mumbai", "New York")
+    print(shortest_path)
