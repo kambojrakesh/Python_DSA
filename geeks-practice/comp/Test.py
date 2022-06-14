0edc6b003083071a0f00b0bb223c3976561e563e
KambojRakesh <rakeshkamboj91@gmail.com>
2022-06-14 19:28:22 -0400

    Work

--- geeks-practice/comp/Test.py
+++ geeks-practice/comp/Test.py
@@ -1,7 +1,7 @@
-stocks = ['reliance', 'infosys', 'tcs']
-prices = [2175, 1127, 2750]
- 
-new_dict = {stocks: prices for stocks,
-            prices in zip(stocks, prices)}
-print(new_dict)
+from collections import Counter
+test_list = [4, 6, 6, 4, 2, 2, 4, 4, 8, 5, 8]
 
+
+
+res = {key : [key] * val for key, val in Counter(test_list).items()}
+print(res)
