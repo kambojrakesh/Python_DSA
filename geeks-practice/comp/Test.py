c322a63070b0e153fd67a6081b48d82a6f1979c9
KambojRakesh <rakeshkamboj91@gmail.com>
2022-06-19 21:49:46 -0400

    Work

--- geeks-practice/comp/Test.py
+++ geeks-practice/comp/Test.py
@@ -1,18 +1,7 @@
-val = [1, 1, 1, 5, 5, 3, 1, 3, 3, 1, 4, 4, 4, 2, 2, 2, 2]
+import collections
+  
+ 
+# Creating a regular dictionary
+d = {chr(k): k for k in range(ord('a') , ord('g'))}
 
-
-from collections import Counter 
-counter = Counter()
-
-dc = {}
-for i in val:
-    dc[i] = val.count(i)
-    
-print(dc)
-
-
-count = {1:8}
-for i in val:
-    count[i] = count.get(i) 
-    
-print(count)
+print(d)
