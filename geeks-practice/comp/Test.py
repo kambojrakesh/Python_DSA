45700c5b4ecb934c965a6755fdeff767b9a6b724
KambojRakesh <rakeshkamboj91@gmail.com>
2022-06-28 16:10:20 -0400

    Work

--- geeks-practice/comp/Test.py
+++ geeks-practice/comp/Test.py
@@ -1,7 +1,10 @@
-import collections
-  
- 
-# Creating a regular dictionary
-d = {chr(k): k for k in range(ord('a') , ord('g'))}
-
-print(d)
+from collections import defaultdict  
+test_dict = {'gfg' : {'x' : 5, 'y' : 6}, 'is' : {'x' : 1, 'y' : 4},
+                                      'best' : {'x' : 8, 'y' : 3}}
+#[(‘x’, (5, 1, 8)), (‘y’, (6, 4, 3))]
+res = defaultdict(tuple)
+for key, val in test_dict.items():
+    for ele in val:
+        res[ele] += (val[ele], )
+        
+print(str(list(res.items())))
